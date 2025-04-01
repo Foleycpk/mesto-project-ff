c3168acb2175d78cae30a9220d318208c99e35b7
mchaplynskikh <foleycpk@gmail.com>
2025-04-02 00:05:36 +0300

    реализовано добавление и удаление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,29 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const page = document.querySelector(".page");
+const placesList = page.querySelector(".places__list");
+
+function addCard(cardTitleValue, cardImageLink, deleteCard) {
+  const card = page.querySelector("#card-template").content;
+  const cardElement = card.querySelector(".card").cloneNode(true);
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__title").textContent = cardTitleValue;
+  cardElement.querySelector(".card__image").src = cardImageLink;
+  cardElement.querySelector(".card__image").alt = cardTitleValue;
+
+  cardDeleteButton.addEventListener("click", function () {
+    deleteCard(cardDeleteButton.closest(".card"));
+  });
+
+  placesList.append(cardElement);
+}
+
+function deleteCard(card) {
+  card.remove();
+}
+
+initialCards.forEach(function (card) {
+  addCard(card.name, card.link, deleteCard);
+});
