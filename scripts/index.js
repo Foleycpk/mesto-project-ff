f249e253921f1b19f91f058da789fefe6a14c28b
mchaplynskikh <foleycpk@gmail.com>
2025-04-02 21:01:06 +0300

    внесены правки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -10,26 +10,32 @@
 const page = document.querySelector(".page");
 const placesList = page.querySelector(".places__list");
 
-function addCard(cardTitleValue, cardImageLink, deleteCard) {
+function createCard(cardTitleValue, cardImageLink, deleteCard) {
   const card = page.querySelector("#card-template").content;
   const cardElement = card.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
   const cardDeleteButton = cardElement.querySelector(".card__delete-button");
 
+  cardImage.src = cardImageLink;
+  cardImage.alt = cardTitleValue;
   cardElement.querySelector(".card__title").textContent = cardTitleValue;
-  cardElement.querySelector(".card__image").src = cardImageLink;
-  cardElement.querySelector(".card__image").alt = cardTitleValue;
 
   cardDeleteButton.addEventListener("click", function () {
     deleteCard(cardDeleteButton.closest(".card"));
   });
 
-  placesList.append(cardElement);
+  return cardElement;
 }
 
 function deleteCard(card) {
   card.remove();
 }
 
+function addCard(card, method = "append") {
+  const cardElement = createCard(card.name, card.link, deleteCard);
+  placesList[method](cardElement);
+}
+
 initialCards.forEach(function (card) {
-  addCard(card.name, card.link, deleteCard);
+  addCard(card);
 });
